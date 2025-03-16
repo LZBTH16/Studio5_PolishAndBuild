46233d2e0f726062c9a35528dd2673bb6ebade56
Adam Zabenskie <zubs047@gmail.com>
2025-03-15 18:38:02 -0700

    lives script

--- Assets/_Scripts/UI/LivesUI.cs
+++ Assets/_Scripts/UI/LivesUI.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
-
+using System.Collections;
+using DG.Tweening;
+using TMPro;
 public class LivesUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private TextMeshProUGUI currentLives;
+    [SerializeField] private TextMeshProUGUI updateLives;
+    [SerializeField] private Transform livesTextContainer;
+    [SerializeField] private float duration;
+    [SerializeField] private Ease animationCurve;
+    private float containerInitPosition;
+    private float moveAmount;
+
+
     void Start()
     {
-        
+        Canvas.ForceUpdateCanvases();
+        currentLives.SetText("3");
+        updateLives.SetText("2");
+        containerInitPosition = livesTextContainer.localPosition.y;
+        moveAmount = currentLives.rectTransform.rect.height;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateLives(int lives)
     {
-        
+        updateLives.SetText($"{ lives }");
+        livesTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration).SetEase(animationCurve);
+        StartCoroutine(ResetLivesContainer(lives));
     }
+
+    private IEnumerator ResetLivesContainer(int lives)
+    {
+        yield return new WaitForSeconds(duration);
+        currentLives.SetText($"{lives}"); // update the original score
+        Vector3 localPosition = livesTextContainer.localPosition;
+        livesTextContainer.localPosition = new Vector3(localPosition.x, containerInitPosition, localPosition.z);
+    }
+
 }
